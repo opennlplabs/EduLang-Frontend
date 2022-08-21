f6741a4daada42c9d02a5397f3a635a8bd5abdd0
Eshaan Barkataki <39027204+Eshaancoding@users.noreply.github.com>
2022-08-21 10:25:24 -0400

    Swiping functionality in bookreader.js

--- Screens/BookReader.js
+++ Screens/BookReader.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, View, Text, ImageBackground, TouchableOpacity} from "react-native";
+import { StyleSheet, View, Text, ImageBackground, TouchableOpacity } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
-import { Image, Dimensions} from 'react-native';
+import { Image, Dimensions } from 'react-native';
 
 const PDFExample = ({ route }) => {
   const { item } = route.params;
@@ -10,8 +10,25 @@ const PDFExample = ({ route }) => {
   const [rightButtonDisabled, setRightButtonDisabled] = useState(false)
   const [height, setHeight] = useState(0)
   const lenPages = Object.keys(item.book).length
+  const [touchX, setTouchX] = useState(0)
 
   useEffect(() => {
+    if (lenPages == 1) {
+      setLeftButtonDisabled(true)
+      setRightButtonDisabled(true)
+    }
+    else if (page <= 1) {
+      setLeftButtonDisabled(true)
+      setRightButtonDisabled(false)
+    }
+    else if (page >= lenPages) {
+      setLeftButtonDisabled(false)
+      setRightButtonDisabled(true)
+    }
+    else {
+      setLeftButtonDisabled(false)
+      setRightButtonDisabled(false)
+    }
     Image.getSize(`data:image/png;base64,${item.book["page1"]}`, (width, height) => {
       const div = height / width
       const windowWidth = Dimensions.get("window").width
@@ -19,17 +36,21 @@ const PDFExample = ({ route }) => {
     });
   }, [Dimensions.get("window")])
 
-  function changePage (increment) {
+  function changePage(increment) {
     var new_page = 0
     if (increment && page < lenPages) {
       setPage(page => page + 1)
       new_page = page + 1
-    } 
+    }
     else if (!increment && page > 1) {
       setPage(page => page - 1)
       new_page = page - 1
     }
-    if (new_page <= 1) {
+    if (lenPages == 1) {
+      setLeftButtonDisabled(true)
+      setRightButtonDisabled(true)
+    }
+    else if (new_page <= 1) {
       setLeftButtonDisabled(true)
       setRightButtonDisabled(false)
     }
@@ -44,29 +65,36 @@ const PDFExample = ({ route }) => {
   }
 
   return (
-  <>
-    <View style={styles.container}>
-      <ImageBackground style={{
-        width: '100%',
-        height: height,
-        marginBottom: 80
-      }} source={{
-        uri: `data:image/jpeg;base64,${item.book["page"+page.toString()]}`,
-      }} />
-    </View>
-
-    <View style={styles.bar}>
-      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <TouchableOpacity disable={leftButtonDisabled} style={styles.Button} onPress={() => changePage(false)}>
-          <AntDesign name="arrowleft" size={24} color={leftButtonDisabled ? "grey" : "black"} />
-        </TouchableOpacity>
-        <Text style={styles.PageNumDisplay}>Page {page}/{lenPages}</Text>
-        <TouchableOpacity disabled={rightButtonDisabled} style={styles.Button} onPress={() => changePage(true)}>
-          <AntDesign name="arrowright" size={24} color={rightButtonDisabled ? "grey" : "black"} />
-        </TouchableOpacity>
+    <>
+      <View style={styles.container}
+        onTouchStart={e => setTouchX(e.nativeEvent.pageX)}
+        onTouchEnd={e => {
+          const diff = touchX - e.nativeEvent.pageX
+          if (diff > 50) changePage(true)
+          else if (diff < -50) changePage(false)
+        }}
+      >
+        <ImageBackground style={{
+          width: '100%',
+          height: height,
+          marginBottom: 80
+        }} source={{
+          uri: `data:image/jpeg;base64,${item.book["page" + page.toString()]}`,
+        }} />
       </View>
-    </View>
-  </>
+
+      <View style={styles.bar}>
+        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+          <TouchableOpacity disable={leftButtonDisabled} style={styles.Button} onPress={() => changePage(false)}>
+            <AntDesign name="arrowleft" size={24} color={leftButtonDisabled ? "grey" : "black"} />
+          </TouchableOpacity>
+          <Text style={styles.PageNumDisplay}>Page {page}/{lenPages}</Text>
+          <TouchableOpacity disabled={rightButtonDisabled} style={styles.Button} onPress={() => changePage(true)}>
+            <AntDesign name="arrowright" size={24} color={rightButtonDisabled ? "grey" : "black"} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
   )
 };
 
@@ -82,7 +110,7 @@ const styles = StyleSheet.create({
   bar: {
     position: 'absolute',
     bottom: 0,
-    left: 0, 
+    left: 0,
     width: '100%',
     height: 80,
     backgroundColor: 'white'
