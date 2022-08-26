7054b95ff561ee857a6c522ee59f33896817dfa5
Eshaan Barkataki <39027204+Eshaancoding@users.noreply.github.com>
2022-08-25 21:53:15 -0400

    Custom Translation

--- backend/src/Paragraph.py
+++ backend/src/Paragraph.py
@@ -1,59 +1,63 @@
 from PIL import Image    # Image class for getting dominant color
-import re                # For some reason pytesseract adds in \n and \x0c. This will remove it
-import pytesseract       # OCR 
+# For some reason pytesseract adds in \n and \x0c. This will remove it
+import re
+import pytesseract       # OCR
 import cv2               # For drawing circles and rectangles on image
 import requests          # For requesting translation to server.
 import json              # For extraction of translation from server
-import src.SensitiveInfo     # Sensitive Info (ex: auth token) regarding connection to the server
-from copy import deepcopy# For deep copying images
+# Sensitive Info (ex: auth token) regarding connection to the server
+import src.SensitiveInfo
+from copy import deepcopy  # For deep copying images
 from time import sleep   # For creating downtime between each API request
 
-class BoundingBox: 
+
+class BoundingBox:
     x = -1
-    y = -1 
+    y = -1
     w = -1
     h = -1
 
-    def __init__ (self, x=-1, y=-1, w=-1, h=-1): 
+    def __init__(self, x=-1, y=-1, w=-1, h=-1):
         self.x = round(x)
         self.y = round(y)
-        self.w = round(w) 
+        self.w = round(w)
         self.h = round(h)
 
-    def __str__ (self):
+    def __str__(self):
         return "x: " + str(self.x) + " y: " + str(self.y) + " w: " + str(self.w) + " h: " + str(self.h)
 
-    def apply_offset (self, offset_x=0, offset_y=0): 
+    def apply_offset(self, offset_x=0, offset_y=0):
         self.x += offset_x
         self.y += offset_y
 
-    def crop_img (self, img):
+    def crop_img(self, img):
         return img[self.y:self.y+self.h, self.x:self.x+self.w]
 
-    def drawBoundingBox (self, img): 
-        return cv2.rectangle(img, (self.x, self.y), (self.x + self.w, self.y + self.h), (0,0,0), 5)
+    def drawBoundingBox(self, img):
+        return cv2.rectangle(img, (self.x, self.y), (self.x + self.w, self.y + self.h), (255, 0, 0), 5)
 
-    def out_of_bounds (self, img_w, img_h, margin=0):
-        if (self.x + self.w > img_w + margin) or (self.y + self.h > img_h + margin) or self.x < 0 or self.y < 0: 
+    def out_of_bounds(self, img_w, img_h, margin=0):
+        if (self.x + self.w > img_w + margin) or (self.y + self.h > img_h + margin) or self.x < 0 or self.y < 0:
             return True
         return False
-    
-class Paragraph: 
-    paragraphBox : BoundingBox = None # BoundingBox of general Paragraph
-    paragraphId = -1    # The paragraph unique id in the page
-    dominant_color = [] # The rgb values of the dominant color
-    translated = "" # The translated text of the paragraph  
 
-    def __init__ (self, TranslatedText, origText, bbox): 
+
+class Paragraph:
+    paragraphBox: BoundingBox = None  # BoundingBox of general Paragraph
+    paragraphId = -1    # The paragraph unique id in the page
+    dominant_color = []  # The rgb values of the dominant color
+    translated = ""  # The translated text of the paragraph
+
+    def __init__(self, TranslatedText, origText, bbox):
         self.translatedText = TranslatedText
         self.originalText = origText
         self.paragraphBox = bbox
 
         # -------------- CHANGE THIS TO YOUR TESSERACT OCR FILE -------------- #
-        pytesseract.pytesseract.tesseract_cmd = "C:\\msys64\\mingw32\\bin\\tesseract.exe" 
+        pytesseract.pytesseract.tesseract_cmd = "C:\\msys64\\mingw32\\bin\\tesseract.exe"
         # -------------------------------------------------------------------- #
 
-    def removeChar(self, text): 
+    def removeChar(self, text):
         special_char_dict = {
             "’": "'",
             "”": "'",
@@ -63,7 +67,7 @@ class Paragraph:
         for key in special_char_dict:
             text = text.replace(key, special_char_dict[key])
 
-    def get_dominant_color (self, image):
+    def get_dominant_color(self, image):
         # Get dominant color
         cropped = Image.fromarray(self.paragraphBox.crop_img(image))
         cropped.thumbnail((100, 100))
@@ -72,22 +76,25 @@ class Paragraph:
         color_counts = sorted(paletted.getcolors(), reverse=True)
         palette_index = color_counts[0][1]
         self.dominant_color = palette[palette_index*3:palette_index*3+3]
-        
-        return self.dominant_color     
 
-    def apply_offset (self, offset_x=0, offset_y=0):
-        self.paragraphBox.apply_offset(offset_x, offset_y)        
+        return self.dominant_color
+
+    def apply_offset(self, offset_x=0, offset_y=0):
+        self.paragraphBox.apply_offset(offset_x, offset_y)
         return self
 
-    def out_of_bounds (self, img_w, img_h):
+    def out_of_bounds(self, img_w, img_h):
         return self.paragraphBox.out_of_bounds(img_w, img_h)
 
-    def draw_text_box (self, img):
-        img = cv2.putText(img, self.originalText, (self.paragraphBox.x, self.paragraphBox.y), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0,0,255), thickness=2) 
-        img = cv2.rectangle(img, (self.paragraphBox.x, self.paragraphBox.y), (self.paragraphBox.x + self.paragraphBox.w, self.paragraphBox.y + self.paragraphBox.h), (0,0,255), 3)
+    def draw_text_box(self, img):
+        img = cv2.putText(img, self.originalText, (self.paragraphBox.x, self.paragraphBox.y),
+                          fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 0, 255), thickness=2)
+        img = cv2.rectangle(img, (self.paragraphBox.x, self.paragraphBox.y), (self.paragraphBox.x +
+                            self.paragraphBox.w, self.paragraphBox.y + self.paragraphBox.h), (0, 0, 255), 3)
         return img
 
-class Page: 
+
+class Page:
     paragraphs = []
     original_image = None
     page_num = 0
@@ -96,15 +103,15 @@ class Page:
         self.paragraphs = paragraphs
         self.original_image = original_image
         self.page_num = page_num
-        for para in self.paragraphs: 
+        for para in self.paragraphs:
             para.get_dominant_color(original_image)
 
-    def display_page (self, name="Display"):
+    def display_page(self, name="Display"):
         img = deepcopy(self.original_image)
         for paragraph in self.paragraphs:
             img = paragraph.draw_text_box(img)
 
         cv2.namedWindow(name, cv2.WINDOW_NORMAL)
         cv2.resizeWindow(name, 1000, 800)
-        cv2.imshow(name, img) 
+        cv2.imshow(name, img)
         cv2.waitKey(0)
--- backend/src/changeImage.py
+++ backend/src/changeImage.py
@@ -2,131 +2,147 @@ import cv2                # Image processing and countour detection
 import os                 # Path
 from tqdm import tqdm     # Progress Bar
 import math               # Math operations for calculating contrast and floor function
-from PIL import ImageDraw # Library for drawing
-from PIL import ImageFont # Library for drawing 
+from PIL import ImageDraw  # Library for drawing
+from PIL import ImageFont  # Library for drawing
 from PIL import Image     # Library for drawing
 import numpy as np        # For image conversion
 from src.Paragraph import *   # Import Paragraph, BoundingBox, and Page classes
-from typing import cast   # To enable accurate and helpful autocomplete during developing :) 
-from copy import deepcopy # For deepcopying the bounding boxes for comparison
+# To enable accurate and helpful autocomplete during developing :)
+from typing import cast
+from copy import deepcopy  # For deepcopying the bounding boxes for comparison
 import base64
 from io import BytesIO
 
 # https://stackoverflow.com/questions/9733288/how-to-programmatically-calculate-the-contrast-ratio-between-two-colors
-def luminance (r, g, b): 
+
+
+def luminance(r, g, b):
     array = []
     for a in [r, g, b]:
         a /= 255
-        if a <= 0.03928: 
+        if a <= 0.03928:
             a /= 12.92
-        else: 
+        else:
             a = math.pow(((a + 0.055) / 1.055), 2.4)
         array.append(a)
     return array[0] * 0.2126 + array[1] * 0.7152 + array[2] * 0.0722
 
-def contrast (rgb1, rgb2): 
+
+def contrast(rgb1, rgb2):
     lum1 = luminance(rgb1[0], rgb1[1], rgb1[2])
     lum2 = luminance(rgb2[0], rgb2[1], rgb2[2])
     brightest = max(lum1, lum2)
     darkest = min(lum1, lum2)
     return (brightest + 0.05) / (darkest + 0.05)
 
+
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
-def changeImage (page): 
-    page = cast(Page, page) 
+
+def changeImage(page):
+    page = cast(Page, page)
     img = page.original_image
-    img_h = img.shape[0] 
-    img_w = img.shape[1]  
+    img_h = img.shape[0]
+    img_w = img.shape[1]
 
     print("Len paragraphs " + str(len(page.paragraphs)))
     for orig_paragraph in page.paragraphs:
         #================ Find the Appropiate Bounding Box to Place next to original Text ================#
-        
+
         # get all potential boxes in all four directions
-        left_para = deepcopy(orig_paragraph).apply_offset(offset_x=-orig_paragraph.paragraphBox.w)
-        right_para = deepcopy(orig_paragraph).apply_offset(offset_x=orig_paragraph.paragraphBox.w)
-        top_para = deepcopy(orig_paragraph).apply_offset(offset_y=-orig_paragraph.paragraphBox.h)
-        bottom_para = deepcopy(orig_paragraph).apply_offset(offset_y=orig_paragraph.paragraphBox.h)
+        left_para = deepcopy(orig_paragraph).apply_offset(
+            offset_x=-orig_paragraph.paragraphBox.w)
+        right_para = deepcopy(orig_paragraph).apply_offset(
+            offset_x=orig_paragraph.paragraphBox.w)
+        top_para = deepcopy(orig_paragraph).apply_offset(
+            offset_y=-orig_paragraph.paragraphBox.h)
+        bottom_para = deepcopy(orig_paragraph).apply_offset(
+            offset_y=orig_paragraph.paragraphBox.h)
         options = [left_para, right_para, top_para, bottom_para]
 
         # Elimate all boxes that are out of bounds from image
-        elim = [] 
-        for option in options: 
+        elim = []
+        for option in options:
             if option.out_of_bounds(img_w, img_h):
                 elim.append(option)
-        for el in elim: 
+        for el in elim:
             options.remove(el)
-        
-        # If there's no boxes left, then continue 
+
+        # If there's no boxes left, then continue
         if len(options) == 0:
             continue
 
         # For the rest of the boxes, assign a score to them that has the most average color closer to paragraph dominant color
-        lowest_score = -1 
+        lowest_score = -1
         best_para = None
-        for option in options: 
+        for option in options:
             cropped_img = option.paragraphBox.crop_img(img)
-            avg_color = np.average(cropped_img, axis=(0,1)).tolist()
+            avg_color = np.average(cropped_img, axis=(0, 1)).tolist()
             score = contrast(avg_color, orig_paragraph.dominant_color)
-            if score < lowest_score or lowest_score == -1: 
+            if score < lowest_score or lowest_score == -1:
                 score = lowest_score
                 best_para = option
         bb_best = best_para.paragraphBox
-        
+
         #============== Add Translated Text to Bounding Box ==============#
-        # Initialize font and text 
-        fontpath = os.path.join(os.getcwd(), "src", "Fonts", "ArialUnicodeMs.ttf")
+        # Initialize font and text
+        fontpath = os.path.join(
+            os.getcwd(), "src", "Fonts", "ArialUnicodeMs.ttf")
         font_size = 50
-        font = ImageFont.truetype(fontpath, font_size, layout_engine=ImageFont.LAYOUT_RAQM)
+        font = ImageFont.truetype(
+            fontpath, font_size, layout_engine=ImageFont.LAYOUT_RAQM)
         entire_text = best_para.translatedText
-        
+
         # Split text if they overflow
-        num_lines = 0 
+        num_lines = 0
         punctuation = " !?.,<>(){}"
         start = 0
         i_break = -1
         for i in range(len(entire_text)):
-            if i == len(entire_text) - 1: 
+            if i == len(entire_text) - 1:
                 break
 
-            if entire_text[i] in punctuation: 
+            if entire_text[i] in punctuation:
                 i_break = i+1
 
             font_bb = font.getsize(entire_text[start:i])
             if font_bb[0] > bb_best.w - 100:
                 start = i
-                entire_text = entire_text[:i_break].rstrip() + "\n" + entire_text[i_break:].lstrip()
-                num_lines += 1 
+                entire_text = entire_text[:i_break].rstrip(
+                ) + "\n" + entire_text[i_break:].lstrip()
+                num_lines += 1
 
-            i+=1
+            i += 1
 
         # If the height of the text is too much, then decrease font
         font_bb = font.getsize(entire_text)
-        while font_bb[1] * num_lines > bb_best.h - 50: 
+        while font_bb[1] * num_lines > bb_best.h - 50:
             font_size -= 1
             font = ImageFont.truetype(fontpath, font_size)
             font_bb = font.getsize(entire_text)
 
-
         # Fill in background color
-        img[bb_best.y : bb_best.y + bb_best.h, bb_best.x : bb_best.x + bb_best.w, 0] = best_para.dominant_color[0]
-        img[bb_best.y : bb_best.y + bb_best.h, bb_best.x : bb_best.x + bb_best.w, 1] = best_para.dominant_color[1]
-        img[bb_best.y : bb_best.y + bb_best.h, bb_best.x : bb_best.x + bb_best.w, 2] = best_para.dominant_color[2]
+        img[bb_best.y: bb_best.y + bb_best.h, bb_best.x: bb_best.x +
+            bb_best.w, 0] = best_para.dominant_color[0]
+        img[bb_best.y: bb_best.y + bb_best.h, bb_best.x: bb_best.x +
+            bb_best.w, 1] = best_para.dominant_color[1]
+        img[bb_best.y: bb_best.y + bb_best.h, bb_best.x: bb_best.x +
+            bb_best.w, 2] = best_para.dominant_color[2]
 
         # Find which color best contrasts dominant color
-        color = (0,0,0)
-        if contrast([255, 255, 255], best_para.dominant_color) > contrast([0,0,0], best_para.dominant_color):
+        color = (0, 0, 0)
+        if contrast([255, 255, 255], best_para.dominant_color) > contrast([0, 0, 0], best_para.dominant_color):
             color = (255, 255, 255)
 
         # Add text
         img_pil = Image.fromarray(img)
         draw = ImageDraw.Draw(img_pil)
         #****************** IMPORTANT TO CHANGE LANGUAGE TO YOUR DESIRED LANGUAGE *************************#
-        draw.text((bb_best.x, bb_best.y), entire_text, fill=color, font=font, language="te")
+        draw.text((bb_best.x, bb_best.y), entire_text,
+                  fill=color, font=font, language="te")
         img = np.array(img_pil)
 
     # Resize the Window
