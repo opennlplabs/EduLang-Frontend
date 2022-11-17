e617cf09264a22eb274db1330d5001ffcc8dbbbd
jovaniq1 <jovaniq1@gmail.com>
2022-11-17 14:52:00 -0600

    added reactNativeTesting library to test UI along with jest, modify yml to check for tests

--- __tests__/canaryTest.test.js
+++ __tests__/canaryTest.test.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { render } from "@testing-library/react-native";
+import Checking from "../Test";
 
 test("canary test", () => {
   const sum = 2 + 2;
-
   expect(sum).toBe(4);
+});
+test("Checks react native testing library is working", () => {
+  const helloWorldText = "Hello World!";
+
+  const { toJSON, getByText } = render(<Checking />);
+
+  const foundHelloWorldText = getByText(helloWorldText);
+
+  expect(foundHelloWorldText.props.children).toEqual(helloWorldText);
+  expect(toJSON()).toMatchSnapshot();
 });
